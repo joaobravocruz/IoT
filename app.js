b5bc3bf5e3f77e3d6a86c868631e8ba661e43293
João Cruz <joaocruz@vetorial.net>
2022-06-19 10:54:31 -0300

    App.js -> node

---
+++ app.js
@@ -0,0 +1,41 @@
+var five = require("johnny-five");
+var firebase = require("firebase");
+var mysql = require("mysql");
+
+var board = new five.Board();
+
+board.on("ready", function() {
+                    var button = new five.Button(2);
+                    board.repl.inject({
+                    button: button
+                  });
+                  
+var led = new five.Led(13);
+  this.repl.inject({
+    led: led
+});
+
+
+// configuração do seu repositorio FireBase
+var config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  storageBucket: "",
+};
+
+firebase.initializeApp(config);
+
+var startCountRef = firebase.database().ref('led').on('value', function(snapshot){
+  let status = snapshot.val();
+
+  if (status == 'on'){
+    led.on()
+    console.log("Led On Remoto");
+
+  }else{
+    led.off()
+    console.log("Led Off Remoto")
+  }
+});
+});
